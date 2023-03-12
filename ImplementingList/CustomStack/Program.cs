8042d88e427584f77e5461af4485dd53dd34b162
Dari <serhi1334@gmail.com>
2023-03-12 11:35:21 +0200

    Custom_List_And_CustomStack

---
+++ ImplementingList/CustomStack/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    class Stack<T> : IEnumerable<T>
+    {
+        private T[] items;
+        public int Count { get; private set; }
+        private const int InitialCapacity = 4;
+        public Stack()
+        {
+            items = new T[InitialCapacity];
+        }
+        public void Push(T element)
+        {
+            if (Count == items.Length)
+            {
+                T[] temparray = new T[items.Length * 2];
+                for (int i = 0; i < Count; i++)
+                {
+                    temparray[i] = items[i];
+                }
+                items = temparray;
+            }
+            
+            items[Count++] = element;
+        }
+        public T Pop()
+        {
+            if (Count <= 0)
+            {
+                throw new InvalidOperationException();
+            }
+            var element = items[Count-1];
+            items[Count--] = (T)Convert.ChangeType(0, typeof(T));
+            if (Count <= items.Length / 4)
+            {
+                T[] temparray = new T[items.Length / 2];
+                for (int i = 0; i < Count; i++)
+                {
+                    temparray[i] = items[i];
+                }
+                items = temparray;
+            }
+            return element;
+        }
+        public T Peek()
+        {
+            if (Count <= 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return items[Count - 1];
+        }
+        public void Foreach(Action<T> action) 
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                action(items[i]);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in items)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+            stack.Push("mori");
+            stack.Push("cheski");
+            stack.Push("dopichivole");
+            stack.Push("taktivole");
+            stack.Push("aaaa");
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Count);
+            stack.Foreach(x => Console.Write(x+" "));
+            foreach (var item in stack)
+            {
+
+            }
+        }
+    }
+}
---
+++ ImplementingList/ImplementingList/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace ImplementingList
+{
+    class List<T>
+    {
+        private const int DefaultCapacity = 2;
+        private int[] items;
+        public int Count { get; private set; }
+        public int this[int i]
+        {
+            get
+            {
+                IsInRange(i);
+                return items[i];
+            }
+            set
+            {
+                IsInRange(i);
+                items[i] = value;
+            }
+        }
+
+     
+
+        public List()
+        {
+            this.items = new int[DefaultCapacity];
+        }
+        public void Add(int element)
+        {
+            Resize();
+            items[Count++] = element;
+        }
+
+       
+
+        public void Print()
+        {
+            Console.WriteLine(string.Join(" ",items));
+        }
+        public int RemoveAt(int index)
+        {
+            if (index<0 || index >= this.Count)
+            {
+                throw new IndexOutOfRangeException("Greshen indeks");
+            }
+            int element = items[index];
+            items[index] = 0;
+            //
+            Count--;
+            for (int i = index; i < Count; i++)
+            {
+                items[i] = items[i + 1];
+            }
+            
+            //Shrink
+            if (items.Length/4 >= Count)
+            {
+                int[] tempArray = new int[items.Length / 2];
+                for (int i = 0; i < Count; i++)
+                {
+                    tempArray[i] = items[i];
+                }
+                items = tempArray;
+            }
+            return element;
+        }
+
+        public bool Contains(int element)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (items[i] == element)
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+        public void Swap(int firstIndex,int secondIndex)
+        {
+            
+            if(firstIndex<0 || firstIndex>=Count || secondIndex<0 || secondIndex >= Count)
+            {
+                throw new IndexOutOfRangeException("Greshen indeks");
+            }
+            int temp = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = temp;
+        }
+        private void IsInRange(int i)
+        {
+            if (i < 0 || i >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+        private void Resize()
+        {
+            if (Count == items.Length)
+            {
+                int[] tempArray = new int[items.Length * 2];
+                for (int i = 0; i < items.Length; i++)
+                {
+                    tempArray[i] = items[i];
+                }
+                items = tempArray;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(20);
+            list.Add(10);
+            list.Add(30);
+            list.Add(31);
+            list.Add(32);
+            list.Add(35);
+            list.Add(50);
+            Console.WriteLine(list[1]);
+            list.Print();
+            list.Swap(0, 6);
+            Console.WriteLine(list.Contains(20));
+            list.Print();
+        }
+    }
+}
