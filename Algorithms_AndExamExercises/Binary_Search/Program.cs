79b954d4037a02e0eccda0a320e2ca4cbe2a06da
Dari <serhi1334@gmail.com>
2023-03-27 18:59:40 +0300

    Algorithms and exam Exercises

---
+++ Algorithms_AndExamExercises/Binary_Search/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Binary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int x = 10;
+            Console.WriteLine(Binary_Search(number, x));
+        }
+
+        private static int Binary_Search(int[] number, int x)
+        {
+            int left = 0;
+            int right = number.Length - 1;
+            int mid = 0;
+            while (left <= right)
+            {
+                mid = (right+left) / 2;
+                if (number[mid] == x)
+                {
+                    return mid;
+                   
+                }
+                else if (number[mid] > x)
+                {
+                    right = mid-1;
+                }
+                else
+                {
+                    left = mid+1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Algorithms_AndExamExercises/Selection Sort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace Selection_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[] { 3, 2, 12, -50, 1 };
+            BubbleSort(number);
+            Console.WriteLine(string.Join(" ",number));
+
+            
+        }
+
+        private static void SelectionSort(int[] number)
+        {
+            for (int i = 0; i < number.Length; i++)
+            {
+                int index = i;
+                for (int j = i+1; j < number.Length; j++)
+                {
+                    if (number[j] < number[index])
+                    {
+                        index = j;
+                    }
+                }
+                (number[i], number[index]) = (number[index], number[i]);
+            }         
+        }
+        private static void BubbleSort(int [] numbers)
+        {
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                for (int j = 0; j < numbers.Length-i-1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        /*
+                         int temp = numbers[j];
+                         numbers[j] = numbers[j+1];
+                         numbers[j+1] = temp
+                        */ 
+                        (numbers[j], numbers[j + 1]) = (numbers[j + 1], numbers[j]);
+                    }
+                }
+            }
+        }
+    }
+}
